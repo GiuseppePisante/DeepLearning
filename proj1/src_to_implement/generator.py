f388ef7fe38241414414c20954dac2da4b2ed2a6
martinaraffaelli <martina.raffaelli@mail.polimi.it>
2024-10-28 11:29:22 +0100

    cleaned the code

--- proj1/src_to_implement/generator.py
+++ proj1/src_to_implement/generator.py
@@ -11,18 +11,20 @@ from PIL import Image, ImageOps
 # This input consists of a batch of images and its corresponding labels.
 class ImageGenerator:
     def __init__(self, file_path, label_path, batch_size, image_size, rotation=False, mirroring=False, shuffle=False):
-        # Define all members of your generator class object as global members here.
-        # These need to include:
-        # the batch size
-        # the image size
-        # flags for different augmentations and whether the data should be shuffled for each epoch
-        # Also depending on the size of your data-set you can consider loading all images into memory here already.
-        # The labels are stored in json format and can be directly loaded as dictionary.
-        # Note that the file names correspond to the dicts of the label dictionary.
+        '''
+        Define all members of your generator class object as global members here.
+        These need to include:
+        the batch size
+        the image size
+        flags for different augmentations and whether the data should be shuffled for each epoch
+        Also depending on the size of your data-set you can consider loading all images into memory here already.
+        The labels are stored in json format and can be directly loaded as dictionary.
+        Note that the file names correspond to the dicts of the label dictionary.
+        '''
 
-        self.class_dict = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer', 5: 'dog', 6: 'frog',
-                           7: 'horse', 8: 'ship', 9: 'truck'}
-        #TODO: implement constructor
+        # Set class attributes
+        self.class_dict = {0: 'airplane', 1: 'automobile', 2: 'bird', 3: 'cat', 4: 'deer',
+                           5: 'dog', 6: 'frog', 7: 'horse', 8: 'ship', 9: 'truck'}
         self.file_path = file_path
         self.label_path = label_path
         self.batch_size = batch_size
@@ -32,13 +34,13 @@ class ImageGenerator:
         self.shuffle = shuffle
         self.labels = self._load_labels()
         
-        
         # Get list of image files
         self.image_files = [f for f in os.listdir(self.file_path) if os.path.isfile(os.path.join(self.file_path, f))]
         if self.shuffle:
             np.random.shuffle(self.image_files)
         self.index = 0
         self.epoch = 0
+    
     # Load labels
     def _load_labels(self):
         with open(self.label_path, 'r') as file:
@@ -46,40 +48,38 @@ class ImageGenerator:
         return labels
 
     def next(self):
-        # This function creates a batch of images and corresponding labels and returns them.
-        # In this context a "batch" of images just means a bunch, say 10 images that are forwarded at once.
-        # Note that your amount of total data might not be divisible without remainder with the batch_size.
-        # Think about how to handle such cases
-        #TODO: implement next method
+        '''
+        This function creates a batch of images and corresponding labels and returns them.
+        In this context a "batch" of images just means a bunch, say 10 images that are forwarded at once.
+        Note that your amount of total data might not be divisible without remainder with the batch_size.
+        Think about how to handle such cases
+        '''
+
+        # Create empty images and labes batches
         batch_images = []
         batch_labels = []
 
         for _ in range(self.batch_size):
-            
+
             # Shuffle if self.shuffle is true + every time we move to a new epoch
-            if self.shuffle and self.index == 0:
+            if self.shuffle and self.index:
                 np.random.shuffle(self.image_files)
+
+            # Reset the index, increment the epoch (and shuffle) if we have used all the images
             if self.index >= len(self.image_files):
                 self.index = 0
                 self.epoch += 1
                 if self.shuffle:
                     np.random.shuffle(self.image_files)
 
+            # Get the current image and its path
             image_file = self.image_files[self.index]
             image_path = os.path.join(self.file_path, image_file)
 
-            # Check if the file is a .npy file 
-            if image_file.endswith('.npy'):
-                with open(image_path, 'rb') as f:
-                    image = np.load(f)
-            else:
-                # Read the image using imageio.v2
-                image = imageio.imread(image_path)
-
-            #image = np.load(image_path)
-            #image_key = os.path.splitext(image_file)[0]  # Strip the .npy extension
+            # Load the image    
+            with open(image_path, 'rb') as f:
+                image = np.load(f)
             
-
             # Resize the image using PIL
             image = Image.fromarray(image)
             image = image.resize(self.image_size[:2], Image.Resampling.LANCZOS)
@@ -92,17 +92,16 @@ class ImageGenerator:
                 if np.random.rand() > 0.5:
                     image = np.flipud(image)
 
-             # Apply rotation
+            # Apply rotation
             if self.rotation and self.index == 0:
                 angle = np.random.choice([90, 180, 270])
                 image = np.array(Image.fromarray(image).rotate(angle))
 
-            batch_images.append(image)
-        
             # Use the file name without extension as the key for labels
             image_key = os.path.splitext(image_file)[0]
 
-            #batch_images.append(image)
+            # Append the new image, the label and increment the index
+            batch_images.append(image)
             batch_labels.append(self.labels[image_key])
             self.index += 1
 
@@ -110,40 +109,44 @@ class ImageGenerator:
         return np.array(batch_images), np.array(batch_labels)
 
     def augment(self,img):
-        # this function takes a single image as an input and performs a random transformation
-        # (mirroring and/or rotation) on it and outputs the transformed image
-        #TODO: implement augmentation function
-            
+        '''
+        this function takes a single image as an input and performs a random transformation
+        (mirroring and/or rotation) on it and outputs the transformed image 
+        '''
         return img
 
     def current_epoch(self):
-        # return the current epoch number
+        '''
+        Return the current epoch number
+        '''
         return self.epoch
 
     def class_name(self, label):
-        # This function returns the class name for a specific input
-        #TODO: implement class name function
+        '''
+        This function returns the class name for a specific input
+        '''
         return self.class_dict.get(self.labels[label], "Unknown")
+    
     def show(self):
-        # In order to verify that the generator creates batches as required, this functions calls next to get a
-        # batch of images and labels and visualizes it.
-        #TODO: implement show method
-        # call next method
+        '''
+        In order to verify that the generator creates batches as required, this functions calls next to get a
+        batch of images and labels and visualizes it.
+        '''
+
+        # Call next method
         batch_images, batch_labels = self.next() 
 
-        # get the number of images
+        # Get the number of images
         num = len(batch_images)
 
-        # create the subplots
+        # Create the subplots
         fig, axes = plt.subplots(1, num, figsize = (15, 5))
 
-        # plot i-th image with its label
+        # Plot i-th image with its label
         for i in range(num):
             axes[i].imshow(batch_images[i])
             axes[i].set_title(self.class_name(batch_labels[i]))
             axes[i].axis('off')
-        
         plt.show()
-
         pass
 
--- proj1/src_to_implement/pattern.py
+++ proj1/src_to_implement/pattern.py
@@ -3,15 +3,21 @@ import matplotlib.pyplot as plt
 
 class Checker():
     def __init__(self, resolution, tile_size):
+        # Set class attributes
         self.resolution = resolution
         self.tile_size = tile_size
-        self.output = np.zeros((self.resolution, self.resolution))
+        self.output = None
         
     def draw(self):
+        # Build tile_size x tile_size matrices of ones and zeros
         black = np.zeros((self.tile_size, self.tile_size), dtype=int)
         white = np.ones((self.tile_size, self.tile_size), dtype=int)
+
+        # Construct the odd and even rows
         rowpattern1 = np.tile(np.hstack([black, white]), (1, self.resolution // (2 * self.tile_size)))
         rowpattern2 = np.tile(np.hstack([white, black]), (1, self.resolution // (2 * self.tile_size)))
+
+        # Combine odd and even pattern together into the output
         self.output = np.tile(np.vstack([rowpattern1, rowpattern2]), (self.resolution // (2 * self.tile_size), 1))
         return self.output.copy()
         
@@ -24,19 +30,21 @@ class Checker():
 
 class Circle:
     def __init__(self, resolution=100, radius=20, position=(0, 0)):
+        # Set class attributes
         self.radius = radius
         self.resolution = resolution
         self.position = position
         self.output = None
 
     def draw(self):
+        # Initialize the output to a zero matrix
         self.output = np.zeros((self.resolution, self.resolution), dtype=int)
-        cx, cy = self.position[0], self.position[1]
 
         # Create a grid of (x, y) coordinates
         x, y = np.meshgrid(np.arange(self.resolution), np.arange(self.resolution))
 
         # Calculate the distance from the center
+        cx, cy = self.position[0], self.position[1]
         distance = (x - cx) ** 2 + (y - cy) ** 2
 
         # Set pixels within the circle to 1
@@ -51,15 +59,20 @@ class Circle:
 
 class Spectrum:
     def __init__(self, resolution=100):
+        # Set class attribute
         self.resolution = resolution
 
     def draw(self):
+
+        # Build and initialize the three channels
         self.r = np.linspace(0, 1, self.resolution)
         self.r = np.tile(self.r, (self.resolution, 1))
         self.g = np.linspace(0, 1, self.resolution)
         self.g = np.tile(self.g, (self.resolution, 1)).T
         self.b = np.linspace(1, 0, self.resolution)
         self.b = np.tile(self.b, (self.resolution, 1))
+
+        # Assemble the output
         self.output=np.stack((self.r, self.g, self.b), axis=-1)
         return self.output.copy()
 
